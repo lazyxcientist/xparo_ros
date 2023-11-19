bec01fe6b7930b5d7f68d0dbf0e7618a97a16202
scientist <xpscientist@gmail.com>
2023-11-19 12:33:36 +0530

    arch

---
+++ xparo_ros/xparo.py
@@ -0,0 +1,339 @@
+
+import websocket
+import json
+import threading
+import requests
+import time
+import os
+import datetime
+import time
+import threading
+
+
+xparo_website = '127.0.0.1:8000' # 'xparo-website.onrender.com'
+DEBUG = True
+
+# 'https://'+xparo_website
+
+########### shedule control ###########
+#######################################
+shedule_control_path = os.path.join(os.path.dirname(__file__), 'shedule_control.json')
+shedule_control = {}
+if os.path.exists(shedule_control_path):
+    with open(shedule_control_path) as f:
+        try:
+            shedule_control = json.load(f)
+        except:
+            shedule_control = {}
+if not shedule_control:
+    with open(shedule_control_path, 'w') as f:
+        json.dump(shedule_control, f)
+
+def update_shedule_control(data):
+    if DEBUG:
+        print("updating shedule control  ",data)
+    global shedule_control_path
+    with open(shedule_control_path, 'w') as f:
+        json.dump(data, f)
+
+
+############### error #############
+###################################
+def read_files_in_directory(root_directory, exclude_files=None):
+    if exclude_files is None:
+        exclude_files = []
+
+    data_dict = {}
+
+    # Walk through the root directory and its subdirectories
+    for root, dirs, files in os.walk(root_directory):
+        current_dict = data_dict
+
+        # Create a nested dictionary structure based on the directory hierarchy
+        for dir_name in os.path.relpath(root, root_directory).split(os.path.sep):
+            current_dict = current_dict.setdefault(dir_name, {})
+
+        # Process files in the current directory
+        for filename in files:
+            # Skip files in the exclude list
+            if filename in exclude_files:
+                continue
+
+            filepath = os.path.join(root, filename)
+
+            # Check if it's a file (not a directory)
+            if os.path.isfile(filepath):
+                with open(filepath, 'r') as file:
+                    # Read the contents of the file
+                    file_content = file.read()
+
+                    # Store the content in the nested dictionary with the full path as the key
+                    current_dict[filename] = file_content
+
+    return data_dict
+
+# Example: Read files in the ROS2 log directory excluding 'logger_all.log' and 'events.log'
+ros2_log_directory = 'log'
+exclude_files_list = ['logger_all.log', 'events.log']
+'''
+{'file name': {
+            "build_id":{
+                        "package":{"file_name":"data"}
+                        }
+                }
+}'''
+
+
+
+
+
+
+
+
+############### websocket #############
+#######################################
+
+class Xparo(websocket.WebSocketApp):
+    def __init__(self, *args, **kwargs):
+        super(Xparo, self).__init__(*args, **kwargs)
+
+
+
+class Project():
+    def __init__(self,email,project_key,secret="public"):
+        self.websocket_connected = False
+        self.email = email
+        self.project_key = project_key
+        self.secret = secret
+
+        # calbacks
+        self.remote_callback = None # takes on parameter
+        self.config_callback = None # config_callback take 2 paramenter
+        self.ai_callback = None 
+        self.video_callback = None
+
+
+        self.connection_type = "websocket"
+
+        self.config_path = os.path.join(os.path.dirname(__file__), 'config.json')
+        self.config = {}
+        if os.path.exists(self.config_path):
+            with open(self.config_path) as f:
+                try:
+                    self.config = json.load(f)
+                except:
+                    self.config = {}
+        if not self.config:
+            with open(self.config_path, 'w') as f:
+                json.dump(self.config, f)
+        self.schedule_control = {}
+        self.connect()
+        
+
+
+
+    def connect(self):
+        print('''
+    connencting to ...
+    ██╗░░██╗██████╗░░█████╗░██████╗░░█████╗░
+    ╚██╗██╔╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗
+    ░╚███╔╝░██████╔╝███████║██████╔╝██║░░██║
+    ░██╔██╗░██╔═══╝░██╔══██║██╔══██╗██║░░██║
+    ██╔╝╚██╗██║░░░░░██║░░██║██║░░██║╚█████╔╝
+    ╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░
+        Live   your   DREAMS   parrallely
+        ''')
+        if self.connection_type == "websocket":
+            if not self.websocket_connected:
+                # socket_server = 'ws://xparo-robot-remote.onrender.com/ws/remote/xparo_remote/123456789/robot/'
+                #FIXME: wss://
+                socket_server='ws://'+xparo_website+'/ws/remote/'+str(self.email)+'/'+str(self.secret)+'/'+str(self.project_key)+'/'
+                self.ws = Xparo(str(socket_server),
+                                on_message=self.on_ws_message,
+                                on_error=self.on_ws_error,
+                                on_open=self.on_ws_open,
+                                on_close=self.on_ws_close,
+                                )
+                self.websocket_connected = True
+                threading.Thread(target=self.ws.run_forever).start()
+            else:
+                print("already connected to xparo remote")
+        elif self.connection_type == "rest":
+            threading.Thread(target=self.start_reset_framework).start()
+            # self.start_reset_framework()
+           
+
+    def send(self,message,remote_name="default"):
+        filtered_data = json.dumps({"type":"command","data":message,"remote_name":remote_name})
+        self.private_send(filtered_data)
+
+
+    def private_send(self,message):
+        pass
+        if self.connection_type == "websocket":
+            try:
+                self.ws.send(message)
+            except Exception as e:
+                print(e)
+        elif self.connection_type == "rest":
+            try:
+                api_url = 'https://'+xparo_website+'/remote/api_project_control/'+self.secret+'/'+self.project_key
+                response = requests.post(api_url, data=message,headers={'Content-type': 'application/json'})
+                if response.status_code == 201:
+                    self.on_ws_message('self.ws', response.json())
+                    if DEBUG:
+                        print(f"command sent successfully {message}")
+                    return True
+                else:
+                    print(str(response))
+                    print('''
+Truble shooting:
+    1. internal server error. make sure you provide correct project id and secret key
+    2. check your project id correct ( available in project dashboard home page )
+    3. make sure your secret key is correct and acitvated (if any)
+    
+''')
+                    return False
+            except Exception as e:
+                print(e)
+                print('''
+Truble shooting:
+    1. check your internet connection
+    2. if that not working download latest version of xparo or from github = https://github.com/lazyxcientist/xparo
+    3. try to switch to websocket connection or rest framework
+    
+''')
+                return False
+
+
+    def on_ws_message(self, ws, message):
+        print(message)
+        if self.connection_type == "websocket":
+            if self.remote_callback:
+                self.remote_callback(message)
+        elif self.connection_type == "rest":
+            kk = message.keys()
+            if 'commands' in kk:  #TODO: {'command':[['data','id'] , [], ]}
+                if self.remote_callback:
+                    for i in message['commands']:
+                        try:
+                            self.remote_callback(str(i))
+                        except Exception as e:
+                            print(e)
+            if 'schedule_control' in kk:
+                update_shedule_control(message['schedule_control'])
+            if 'change_config' in kk:
+                for i,j in message['change_config'].items():
+                    self.update_config(i,j)
+                    if self.config_callback:
+                        try:
+                            self.config_callback(i,j)
+                        except Exception as e:
+                            print(e)
+            if 'ai_bot' in kk: 
+                if self.ai_callback:
+                    for i in message['ai_bot']:
+                        try:
+                            self.ai_callback(str(i))
+                        except Exception as e:
+                            print(e)
+            if 'tele_video' in kk: 
+                if self.video_callback:
+                    for i in message['tele_video']:
+                        try:
+                            self.video_callback(str(i))
+                        except Exception as e:
+                            print(e)
+            if 'error' in kk:
+                self.send_error()
+            if 'core' in kk:
+                result = eval(message['core'])
+                self.private_send(json.dumps({"type":"core_result","data":str(result)}))
+
+    def on_ws_error(self, ws, error):
+        print(error)
+
+    # def ws_connection(self, dt, **kwargs):
+    #     print("connecting to websocket")
+    #     threading.Thread(target=self.ws.run_forever).start()
+
+
+    def on_ws_open(self, ws):
+        self.websocket_connected = True
+        print('''
+        \\\\Connection Sussessfull//
+           \\\\X.P.A.R.O remote//
+            \\\\is 🄻🄸🅅🄴 now//
+        
+        ''')
+
+
+    def on_ws_close(self, *args):
+        self.websocket_connected = False
+        print('''
+    Connection with XPARO  is
+    █▀▀ █── █▀▀█ █▀▀ █▀▀ █▀▀▄ 
+    █── █── █──█ ▀▀█ █▀▀ █──█ 
+    ▀▀▀ ▀▀▀ ▀▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀─
+        retry again !!!
+
+        ''')
+        time.sleep(10)
+        self.connect()
+
+
+    def start_reset_framework(self):
+        print("starting reset framework")
+        check = self.private_send(json.dumps({"config":self.config, "initiliaze":True}))
+        self.send_error()
+        if check:
+            while True:
+                response = requests.get('https://'+xparo_website+'/remote/api_project_control/'+self.secret+'/'+self.project_key)
+                if response.status_code == 201:
+                    data = response.json()
+                    self.on_ws_message('self.ws',data)
+
+                time.sleep(0.1)
+        else:
+            print("unable to connect with X.P.A.R.O server")
+            self.on_ws_close('self.ws')
+
+
+    ############################
+    ###### custom send #########
+    ############################
+    
+    def send_error(self):
+        try:
+            errors = read_files_in_directory(ros2_log_directory, exclude_files=exclude_files_list)
+            self.private_send(json.dumps({"program_bugs":errors}))
+        except Exception as e:
+            print(e)
+
+
+    def update_config(self,key,value):
+        self.config[key] = value
+        with open(self.config_path, 'w') as f:
+            json.dump(self.config, f)
+        if self.config_callback:
+            try:
+                self.config_callback(key,value)
+            except Exception as e:
+                print(e)
+        self.private_send(json.dumps({"config":self.config}))
+
+
+
+# Wait for the monitor thread to finish gracefully
+# monitor_thread.join() #FIXME: uncomment it for error tracking
+
+
+if __name__ == "__main__":
+
+    remote = Project("test_remote","e99dd21e-f7b2-4a4c-a74f-4658ad4dd2bd")
+
+    def remote_callback(message):
+        print(message)
+    remote.remote_callback = remote_callback
+
+    remote.send("hello")
---
+++ xparo_ros/xparo_node.py
@@ -0,0 +1,96 @@
+import rclpy
+from rclpy.node import Node 
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from .xparo import Project
+from datetime import datetime
+from dateutil import parser
+
+
+
+
+
+class Xparo_node(Node):
+    def __init__(self, **kwargs):
+        super().__init__("xparo")
+        self.create_timer(1.0, self.timmer_fun) # for running shedule control commands
+
+        # remote commmand send and receive for teleoperation
+        self.remote_receive = self.create_publisher(String,"/xparo/remote",10)
+        self.remote_send = self.create_subscription(String,"/xparo/remote/send",self.remote_command_reviced, 10)
+
+        # make global configuration which you can change remotely
+        self.change_config = self.create_publisher(String,"/xparo/config",10) 
+        self.change_config = self.create_subscription(String,"/xparo/config/send", self.config_recive,10) 
+
+        # interact with realworld ai chatbot to make your robot smart
+        self.ai_receive = self.create_publisher(String,"/xparo/ai", 10)
+        self.ai_send = self.create_subscription(String,"/xparo/ai/send",self.ai_chatbot_command , 10)
+
+        # send video frame remotely
+        self.video_receive = self.create_publisher(Image,"/xparo/video", 10)
+        self.video_send = self.create_subscription(Image,"/xparo/video/send",self.revive_video_frame , 10)
+
+
+        #################################
+        ############ X.P.A.R.O ##########
+        email = 'test_remote'
+        project_key = 'd6da039f-06ae-4ccd-a2fc-2f817caae6de'
+        secret = 'public'
+        self.project = Project(email,project_key,secret)
+        self.project.remote_callback = self.remote_callback
+        self.project.config_callback = self.config_callback
+        self.project.ai_callback = self.ai_callback
+        self.project.video_callback = self.video_callback
+
+
+
+
+    def remote_callback(self,msg):
+        ss = String()
+        ss.data = str(msg)
+        self.remote_receive.publish(ss)
+    def config_callback(self,key,val):
+        ss = String()
+        ss.data = str({key:val})
+        self.change_config.publish(ss)
+    def ai_callback(self,msg):
+        ss = String()
+        ss.data = str(msg)
+        self.ai_receive.publish(ss)
+    def video_callback(self,msg):
+        ss = Image()
+        ss.data = str(msg)
+        self.video_receive.publish(ss)
+
+
+    def remote_command_reviced(self,message):
+        self.project.send(str(message.data))
+    def ai_chatbot_command(self, message):
+        self.project.private_send({"type":"ai","data":str(message.data)})
+    def revive_video_frame(self,message):
+        self.project.private_send({"type":"media","data":str(message.data)})
+    def config_recive(self,message):
+        self.project.private_send({"type":"config","data":str(message.data)})
+
+
+        
+    def timmer_fun(self,**kwargs):
+        current_time = datetime.now()
+        for command, details in self.project.schedule_control.items():
+            scheduled_time = parser.parse(details['date'] + ' ' + details['time'])
+            if current_time >= scheduled_time:
+                self.get_logger().info(f"Executing scheduled command '{command}' at {current_time}")
+                ss = String()
+                ss.data = str(command)
+                self.remote_receive.publish(ss)
+        
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    rclpy.spin(Xparo_node())
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
